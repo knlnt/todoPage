664172a2379ad3c4c8f5e30954939ad77677b8a5
KNLNT <apovolnov@gmail.com>
2019-08-16 16:57:12 +0500

    build app

---
+++ precache-manifest.6c943ffdf3b51388cb495c971142598b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8491ae14428077e56bb0cf6ee46152d",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "0d24732794a77ab88137",
+    "url": "/todo/static/css/main.a5a1f9d1.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/todo/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "0d24732794a77ab88137",
+    "url": "/todo/static/js/main.cd5ba2ae.chunk.js"
+  },
+  {
+    "revision": "3ce2d46cb5510c372f4d",
+    "url": "/todo/static/js/runtime~main.7a2dc5ec.js"
+  },
+  {
+    "revision": "648f42e2e56021336041ee19cb28309b",
+    "url": "/todo/static/media/bg.648f42e2.jpg"
+  }
+]);
